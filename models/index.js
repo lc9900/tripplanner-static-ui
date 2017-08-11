82b4ad66ffef9f29cc9717b83253a1bc51689f33
lc9900 <whynot_hm@hotmail.com>
2017-08-10 21:32:07 -0400

    did some models

--- models/index.js
+++ models/index.js
@@ -8,6 +8,64 @@ const sync = ()=> {
   return conn.sync({ force: true });
 };
 
+// hotels: [
+    // {name: "Andaz Wall Street", place: {address: "75 Wall St", city: "New York", state: "NY", phone: "123-456-7890", location: [40.705137, -74.007624]}, num_stars: 4, amenities: "Pool, Free Wi-Fi" },
+
+
+const Restaurant = conn.define('restaurant', {
+   name: {},
+   cuisine: {},
+   price: {}
+});
+
+const Activity = conn.define('activity', {
+    name: {
+        type: Sequelize
+    },
+    age_range: {}
+});
+
+const Place = conn.define('place', {
+    address: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    city:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    state:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    phone:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    location:{
+        type: Sequelize.ARRAY(Sequelize.FLOAT),
+        allowNull: false
+    },
+});
+
+const Hotel = conn.define('hotel', {
+    name:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    num_stars: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    amenities: {
+        type: Sequelize.STRING
+    }
+});
+
+Hotel.belongsTo(Place);
+Activity.belongsTo(Place);
+Restaurant.belongsTo(Place);
+
 module.exports = {
   sync,
   models: {
